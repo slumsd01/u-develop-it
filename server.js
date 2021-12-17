fabb6dd90be822474a3d1737f20bd602e51a916d
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-12-16 19:17:52 -0500

    12.2.4

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,20 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Emmett1991!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {

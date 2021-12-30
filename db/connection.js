c7e27a18a22f8d30bd968445bbb36101667e0ebe
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2021-12-30 09:29:48 -0500

    12.5

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: 'Emmett1991!',
+  password: '',
   database: 'election'
 });
 
--- server.js
+++ server.js
@@ -14,15 +14,14 @@ app.use('/api', apiRoutes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 // Start server after DB connection
 db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
